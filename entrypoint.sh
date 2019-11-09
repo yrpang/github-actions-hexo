0eb7fa6e5993c1c643e8a2cfaae1cfe17860e46b
yrpang <yrpang@outlook.com>
2019-11-10 01:28:22 +0800

    add clear cloudflare cache

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,17 +3,15 @@ set -e
 
 # setup key
 mkdir -p /root/.ssh/
-echo "${INPUT_DEPLOYKEY}" > /root/.ssh/id_rsa
+echo "${INPUT_DEPLOYKEY}" >/root/.ssh/id_rsa
 chmod 600 /root/.ssh/id_rsa
-ssh-keyscan -t rsa github.com >> /root/.ssh/known_hosts
+ssh-keyscan -t rsa github.com >>/root/.ssh/known_hosts
 
 git config --global user.name "${INPUT_USERNAME}"
 git config --global user.email "${INPUT_EMAIL}"
 
-
 # setup hexo env
 npm i -g hexo-cli
-npm install hexo-deployer-git
 npm install
 
 # generate&publish
@@ -35,4 +33,17 @@ if ${INPUT_IF_UPDATE_FILES}; then
         git commit -m "triggle by commit ${GITHUB_SHA}" -a
         git push "${remote_repo}" HEAD:${INPUT_BRANCH}
     fi
+fi
+
+# Purge cache in CloudFlare
+if ${INPUT_IF_UPDATE_CLOUDFLARE}; then
+    [ -z "${INPUT_CLOUDFLARE_API_KEY}" ] && {
+        echo 'Missing input cloudflare api key'
+        exit 1
+    }
+    curl -X POST "https://api.cloudflare.com/client/v4/zones/${INPUT_CLOUDFLARE_ZONE}/purge_cache" \
+        -H "X-Auth-Email: ${INPUT_CLOUDFLARE_USERNAME}" \
+        -H "X-Auth-Key:  ${INPUT_CLOUDFLARE_API_KEY}" \
+        -H "Content-Type: application/json" \
+        --data '{"purge_everything":true}'
 fi
